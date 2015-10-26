cc51825a87f2f1cb35ef705f03e87b5964d4fd45
zhangfanghui <fhzhang@lonshinetech.com>
2015-10-26 10:32:15 +0800

    modify include ,use BASE_URL

--- application/views/shared/footer.php
+++ application/views/shared/footer.php
@@ -2,10 +2,11 @@
 $base_url = $this->config->item('base_url');
 $assets = $this->config->item('assets');
 ?>
-
-<script src="<?=$assets;?>bower_components/jquery/dist/jquery.min.js"></script>
-<script src="<?=$assets;?>bower_components/foundation/js/foundation.min.js"></script>
-<script src="<?=$assets;?>js/app.js"></script>
+<!--
+<script src="<?=BASE_URL;?>/assets/bower_components/jquery/dist/jquery.min.js"></script>
+-->
+<script src="<?=BASE_URL;?>/assets/bower_components/foundation/js/foundation.min.js"></script>
+<script src="<?=BASE_URL;?>/assets/js/app.js"></script>
 
 </body>
 </html>
--- application/views/shared/header.php
+++ application/views/shared/header.php
@@ -10,7 +10,12 @@ $assets = $this->config->item('assets');
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $this->lang->line('title_keyword');?></title>
-    <link rel="stylesheet" href="<?=$assets;?>css/app.css" />
-    <script src="<?=$assets;?>bower_components/modernizr/modernizr.js"></script>
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/css/app.css" />
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/js/trevor/sir-trevor.css" />
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/js/trevor/sir-trevor-bootstrap.css" />
+    <link rel="stylesheet" href="<?=BASE_URL;?>/assets/js/trevor/sir-trevor-icons.css" />
+
+    <script src="<?=BASE_URL;?>/assets/bower_components/modernizr/modernizr.js"></script>
+    <script src="<?=BASE_URL;?>/assets/js/jquery-1.10.2.min.js"></script>
   </head>
   <body>
